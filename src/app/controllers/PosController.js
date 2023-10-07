ccdb1a57801955fc5ba5a94f22701e87098e89b1
Tan Thi <52000806@student.tdtu.edu.vn>
2023-10-07 15:41:42 +0700

    code views pos page

--- src/app/controllers/PosController.js
+++ src/app/controllers/PosController.js
@@ -1,7 +1,7 @@
 class PosController {
     //GET /pos
     pos(req, res) {
-        res.render("pages/pos")
+        res.render("pages/pos", {loop:[1,2,3,4,5,6,7,8,9,12,31321,2313,123,12312,312,3]})
     }
 
     //more
