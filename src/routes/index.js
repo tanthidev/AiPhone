39bc15ffe97ab8eacf22b24416e571bc507868a0
Tan Thi <52000806@student.tdtu.edu.vn>
2023-10-22 21:42:54 +0700

    set route page login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const homeRouter = require('./home');
 const productsRouter = require('./productlist');
 const posRouter = require('./pos')
+const loginRouter = require('./login')
 
 function route(app) {
     app.use('/productlist', productsRouter);
     app.use('/pos', posRouter);
+    app.use('/login', loginRouter)
     app.use('/', homeRouter);
 }
 
