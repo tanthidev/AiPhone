bfe74f93f1f94e9564aedd9c11a3e077c3cc0958
Minh Thuan <52000809@student.tdtu.edu.vn>
2023-10-28 15:48:23 +0700

    chu thich

---
+++ src/app/controllers/InvoiceController.js
@@ -0,0 +1,9 @@
+
+class InvoiceController {
+    //GET /home
+    invoice(req, res) {
+        res.render("pages/invoice",)
+    }
+}
+
+module.exports = new InvoiceController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ const homeRouter = require('./home');
 const productsRouter = require('./productlist');
 const posRouter = require('./pos')
 const loginRouter = require('./login')
+const invoiceRouter = require('./invoice')
+
 
 function route(app) {
     app.use('/productlist', productsRouter);
     app.use('/pos', posRouter);
     app.use('/login', loginRouter)
+    app.use('/invoice', invoiceRouter)
     app.use('/', homeRouter);
 }
 
